d6a7210b4acaf2cff984b398f3500c8f9480661b
wkd3475 <wkd3475@gmail.com>
2019-02-15 17:03:44 +0900

    make link to run well

--- account/urls.py
+++ account/urls.py
@@ -3,5 +3,6 @@ from account import views
 
 urlpatterns = [
     path('', views.post_index, name='post_index'),
-    path('sign_up', views.post_sign_up, name='sign_up')
+    path('post_index', views.post_index, name='post_index'),
+    path('sign_up', views.post_sign_up, name='sign_up'),
 ]

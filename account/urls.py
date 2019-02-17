8d83e435f44c5a8fa8808a8f9ac09fe8e0fd1606
wkd3475 <wkd3475@gmail.com>
2019-02-17 23:21:16 +0900

    change the name, post_index.html -> login.html

--- account/urls.py
+++ account/urls.py
@@ -3,6 +3,6 @@ from account import views
 
 urlpatterns = [
     path('', views.post_index, name='post_index'),
-    path('post_index', views.post_index, name='post_index'),
+    path('login', views.post_index, name='post_index'),
     path('sign_up', views.post_sign_up, name='sign_up'),
 ]
--- account/views.py
+++ account/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def post_index(request):
-    return render(request, 'account/post_index.html', {})
+    return render(request, 'account/login.html', {})
 
 def post_sign_up(request):
     return render(request, 'account/sign_up.html', {})
